ee48daa06c99393fea1a2109b678f7b770674f22
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2025-01-20 22:48:03 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import CheckDetails from "./_mainpage/CheckDetails"
 import Price from "./_mainpage/Price"
 function HomePage() {
   return (
-    <div className="flex md:flex-row flex-col-reverse w-full gap-2 md:gap-5 px-10 md:pb-[calc(66px+80px)] md:py-[66px] pb-[calc(66px)]  h-full">
+    <div className="flex md:flex-row flex-col-reverse w-full gap-2 md:gap-5 px-5 sm:px-10 md:pb-[calc(66px+80px)] md:py-[66px] pb-[calc(66px)]  h-full">
       <div className="flex flex-col md:w-1/2 w-full h-full gap-2 md:gap-5">
         <div className=" flex flex-row h-full gap-2 md:gap-5">
           <WarmAs />
